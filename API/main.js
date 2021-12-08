1777e6f3ad88f2f02e5983e2e8fb385f179a7211
AgnethaDMello <agnethad@nycstudents.net>
2021-12-08 13:57:56 -0500

    testing

--- API/main.js
+++ API/main.js
@@ -1,6 +1,9 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const URL = "https://dog.ceo/api/breeds/image/random";
+
+async function getData(URL) {
+  const response = await fetch(URL);
+  console.log(response);
+}
+getData(URL);
