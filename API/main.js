4516f51ccb5c303e7ee9e7f0964bdecf6324a8d6
AgnethaDMello <agnethad@nycstudents.net>
2021-12-09 13:44:16 -0500

    testing done :D

--- API/main.js
+++ API/main.js
@@ -3,7 +3,12 @@ import './style.css'
 const URL = "https://dog.ceo/api/breeds/image/random";
 
 async function getData(URL) {
-  const response = await fetch(URL);
-  console.log(response);
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
+    document.getElementById("app").innerHTML = data.message;
+  } catch (error) {
+    console.log(error);
+  }
 }
 getData(URL);
