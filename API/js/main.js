e56e45c02a5d50a81f33d8e774472bbfed46b924
Yinglin Guan <90654643+233693423@users.noreply.github.com>
2022-01-04 13:25:50 -0500

    .

--- API/js/main.js
+++ API/js/main.js
@@ -1,6 +1,3 @@
 import "../css/style.css";
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+document.querySelector('#header').innerHTML = `<h1>Dashing Dogs!</h1>`
