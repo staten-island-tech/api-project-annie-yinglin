e2a6cb27423b9cd12696f3f525660abf262eabdb
AgnethaDMello <agnethad@nycstudents.net>
2022-01-04 13:50:19 -0500

    e

--- API/js/main.js
+++ API/js/main.js
@@ -1,3 +1,18 @@
 import "../css/style.css";
 
 document.querySelector('#header').innerHTML = `<h1>Dashing Dogs!</h1>`
+
+const URL = "https://dog.ceo/api/breeds/image/random";
+
+async function getData(URL) {
+    const response = await fetch(URL);
+    console.log(response);
+    try {
+        const response = await fetch(URL);
+        const data = await response.json();
+        document.getElementById("gallery").innerHTML = data.message;
+    } catch (error) {
+        console.log(error);
+    }
+}
+getData(URL);
