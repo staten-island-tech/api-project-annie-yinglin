85b93ebfef8e62f52e532c8c5194dbcbac632c18
AgnethaDMello <agnethad@nycstudents.net>
2022-01-11 18:55:18 -0500

    images added!

--- API/js/main.js
+++ API/js/main.js
@@ -2,7 +2,7 @@ import "../css/style.css";
 
 document.querySelector('#header').innerHTML = `<h1>Dashing Dogs!</h1>`
 
-const URL = "https://dog.ceo/api/breeds/image/random";
+const URL = "https://dog.ceo/api/breed/hound/images";
 
 async function getData(URL) {
     const response = await fetch(URL);
@@ -10,31 +10,14 @@ async function getData(URL) {
     try {
         const response = await fetch(URL);
         const data = await response.json();
-        document.getElementById("gallery").insertAdjacentHTML("afterbegin",
-            `<img class ="display-img" src="${data.message}"/>`
-        )
+        const images = data.message;
+        images.forEach((image) => {
+            document.getElementById("gallery").insertAdjacentHTML("afterbegin",
+                `<img class ="img" src="${image}"/>`)
+        });
+        console.log(data);
     } catch (error) {
         console.log(error);
     }
 }
 getData(URL);
-
-/* const createcards = async () => {
-    try { const apiEntry = await fetch("https://www.breakingbadapi.com/api/characters"); BreakingBadCharacters = await apiEntry.json(); displayCharacters(BreakingBadCharacters); } catch (err) { console.error(err); }
-};
-const displayCharacters = (characters) => {
-    const htmlString = characters.map((character) => {
-        return
-        `<li class="character">
-            <img class="image" src="${character.img}" />
-            <h2 class="name">Name: ${character.name}<h2 />
-                <h3 class="nickname">Nickname: ${character.nickname}<h3 />
-                    <h3 class="birthday">Birthday: ${character.birthday}<h3 />
-                        <h3 class="occupation">Occupation: ${character.occupation[0]}<h3 />
-                            <h3 class="portrayed">Portrayed by: ${character.portrayed}<h3 />
-                                <h3 class="status">Status: ${character.status}<h3 />
-            </li>`
-            ;
-    }).join(""); charactersList.innerHTML = htmlString;
-}; createcards();
- */
