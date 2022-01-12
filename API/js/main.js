87bc6229ba2a54468c8b0247e0adf58c5bb0c30d
AgnethaDMello <agnethad@nycstudents.net>
2022-01-12 13:58:46 -0500

    oh

--- API/js/main.js
+++ API/js/main.js
@@ -3,6 +3,9 @@ import "../css/style.css";
 document.querySelector('#header').innerHTML = `<h1>Dashing Dogs!</h1>`
 
 const URL = "https://dog.ceo/api/breed/hound/images";
+const clearbtn = document.getElementById("clear");
+const searchbar = document.getElementById("search");
+let resultDogs = [];
 
 async function getData(URL) {
     const response = await fetch(URL);
@@ -21,3 +24,70 @@ async function getData(URL) {
     }
 }
 getData(URL);
+
+function clear(searchbar) {
+    clearbtn.addEventListener("click", function (event) {
+        searchbar.value = "";
+    })
+};
+clear(searchbar);
+
+/* function showList(results) {
+    for (const dog of results) {
+        const resultItem = document.createElement('li')
+        resultItem.classList.add('result-item')
+        const text = document.createTextNode(images)
+        resultItem.appendChild(text)
+        list.appendChild(resultItem)
+    }
+}
+
+searchbar.addEventListener("keyup", (e) => {
+    let value = e.target.value
+    if (value && value.trim().length > 0) {
+        value = value.trim().toLowerCase()
+        showList(images.filter(image => {
+            return image.includes(value)
+        }))
+    } else {
+    }
+}) */
+
+searchBar.addEventListener('keyup', (e) => {
+    const searchString = e.target.value.toLowerCase();
+
+    const filteredDogs = resultDogs.filter((dog) => {
+        return (
+            dog.name.toLowerCase().includes(searchString)
+        );
+    });
+    displayCharacters(filteredCharacters);
+});
+
+const loadCharacters = async () => {
+    try {
+        const res = await fetch('https://hp-api.herokuapp.com/api/characters');
+        hpCharacters = await res.json();
+        displayCharacters(hpCharacters);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+const displayCharacters = (characters) => {
+    const htmlString = characters
+        .map((character) => {
+            return `
+            <li class="character">
+                <h2>${character.name}</h2>
+                <p>House: ${character.house}</p>
+                <img src="${character.image}"></img>
+            </li>
+        `;
+        })
+        .join('');
+    charactersList.innerHTML = htmlString;
+};
+
+loadCharacters();
+
